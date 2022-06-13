73cdebe5a86468bdbc31b1b161e25ec6935a45b0
Pavel <paha_gagarin@mail.ru>
2022-06-13 13:23:40 +0300

    проверка на существование переменных

--- src/js/modals.js
+++ src/js/modals.js
@@ -3,27 +3,29 @@ export function modalForm() {
     const triggers = document.querySelectorAll(triggerSelector);
     const modal = document.querySelector(modalSelector);
     const closes = document.querySelectorAll(closeSelector);
-    triggers.forEach(function (trigger) {
-      trigger.addEventListener('click', function () {
-        modal.classList.remove('js-modal-hidden');
-        document.body.style.overflow = "hidden";
+    if(triggers.length && modal && closes.length){
+      triggers.forEach(function (trigger) {
+        trigger.addEventListener('click', function () {
+          modal.classList.remove('js-modal-hidden');
+          document.body.style.overflow = "hidden";
+        });
       });
-    });
-    closes.forEach(function (close) {
-      close.addEventListener('click', function () {
-        modal.classList.add('js-modal-hidden');
-        document.body.style.overflow = "";
+      closes.forEach(function (close) {
+        close.addEventListener('click', function () {
+          modal.classList.add('js-modal-hidden');
+          document.body.style.overflow = "";
+        });
       });
-    });
-    modal.addEventListener('click', function (e) {
-      if (e.target === modal) {
-        modal.classList.add('js-modal-hidden');
-        document.body.style.overflow = "";
-      }
-      if(e.target.classList.contains('js-form-button-modal')){
-        modal.remove()
-      }
-    });
+      modal.addEventListener('click', function (e) {
+        if (e.target === modal) {
+          modal.classList.add('js-modal-hidden');
+          document.body.style.overflow = "";
+        }
+        if(e.target.classList.contains('js-form-button-modal')){
+          modal.remove()
+        }
+      });
+    }
   };
   getModal('.js-card-detail-button', '.js-modal-form', '.js-close-button')
   getModal('.js-form-button-modal', '.js-modal-thanks', '.js-close-button')
